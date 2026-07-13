bfee4e71f250ad6727f7d9a9e3dede20acc97145
JunYoung Choi <juny0955@gmail.com>
2026-07-13 22:19:26 +0900

    feat: TCP Proxy 구현

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,35 @@
-use tokio::{io, net::TcpListener};
+use std::net::SocketAddr;
 
+use tokio::{io, net::{TcpListener, TcpStream}};
 
 #[tokio::main]
 async fn main() -> io::Result<()>{
-    println!("rplb started");
-    let listener = TcpListener::bind("127.0.0.1:8080").await?;
-    println!("listening in 8080");
+    tracing_subscriber::fmt().with_target(false).init();
 
+    let listen_addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let backend_addr = SocketAddr::from(([127, 0, 0, 1], 9000));
+    let listener = TcpListener::bind(listen_addr).await?;
 
-    match listener.accept().await {
-        Ok((_socket, addr)) => println!("new client: {:?}", addr),
-        Err(e) => println!("couldn't get clienct: {:?}", e),
+    tracing::info!(%listen_addr, %backend_addr, "rplb started");
+
+    loop {
+        match listener.accept().await {
+            Ok((client, client_addr)) => {
+                tokio::spawn(async move {
+                    if let Err(e) = handle_connection(client, backend_addr).await {
+                        tracing::warn!(%client_addr, %backend_addr, %e, "proxy connection failed");
+                    }
+                });
+            }
+            Err(e) => {
+                tracing::warn!(%e, "failed to accept client connection");
+            }
+        }
     }
+}
 
+async fn handle_connection(mut client: TcpStream, backend_addr: SocketAddr) -> io::Result<()> {
+    let mut backend = TcpStream::connect(backend_addr).await?;
+    io::copy_bidirectional(&mut client, &mut backend).await?;
     Ok(())
 }
