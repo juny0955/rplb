d5b18ccc47dc0b26b3e30b823e590f657d51e8a5
JunYoung Choi <juny0955@gmail.com>
2026-07-13 21:49:57 +0900

    add tcp listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::{io, net::TcpListener};
+
+
+#[tokio::main]
+async fn main() -> io::Result<()>{
+    println!("rplb started");
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    println!("listening in 8080");
+
+
+    match listener.accept().await {
+        Ok((_socket, addr)) => println!("new client: {:?}", addr),
+        Err(e) => println!("couldn't get clienct: {:?}", e),
+    }
+
+    Ok(())
 }
