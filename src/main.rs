c105355d7390eb55c3f14e7dc9c80833860b9495
JunYoung Choi <juny0955@gmail.com>
2026-07-15 23:18:44 +0900

    feat: TCP proxy 라운드로빈 적용

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::net::SocketAddr;
+use std::{net::SocketAddr, sync::Arc};
 
+use rplb::pool::{PoolError, ServerPool};
 use tokio::{
     io,
     net::{TcpListener, TcpStream},
@@ -10,17 +11,36 @@ async fn main() -> io::Result<()> {
     tracing_subscriber::fmt().with_target(false).init();
 
     let listen_addr = SocketAddr::from(([127, 0, 0, 1], 8080));
-    let backend_addr = SocketAddr::from(([127, 0, 0, 1], 9000));
+
+    let server_addrs = [
+        SocketAddr::from(([127, 0, 0, 1], 9000)),
+        SocketAddr::from(([127, 0, 0, 1], 9001)),
+    ];
+    let server_pool = Arc::new(ServerPool::new(server_addrs.to_vec()));
+
     let listener = TcpListener::bind(listen_addr).await?;
 
-    tracing::info!(%listen_addr, %backend_addr, "rplb started");
+    tracing::info!(%listen_addr, ?server_addrs, "rplb started");
 
     loop {
         match listener.accept().await {
             Ok((client, client_addr)) => {
+                let server_addr = match server_pool.select() {
+                    Ok(addr) => addr,
+                    Err(PoolError::EmptyPool) => {
+                        tracing::warn!(%client_addr, "no server available");
+                        continue;
+                    }
+                };
+
                 tokio::spawn(async move {
-                    if let Err(e) = handle_connection(client, backend_addr).await {
-                        tracing::warn!(%client_addr, %backend_addr, %e, "proxy connection failed");
+                    if let Err(e) = handle_connection(client, server_addr).await {
+                        tracing::warn!(
+                            %client_addr,
+                            %server_addr,
+                            %e,
+                            "proxy connection failed"
+                        );
                     }
                 });
             }
@@ -31,8 +51,8 @@ async fn main() -> io::Result<()> {
     }
 }
 
-async fn handle_connection(mut client: TcpStream, backend_addr: SocketAddr) -> io::Result<()> {
-    let mut backend = TcpStream::connect(backend_addr).await?;
-    io::copy_bidirectional(&mut client, &mut backend).await?;
+async fn handle_connection(mut client: TcpStream, server_addr: SocketAddr) -> io::Result<()> {
+    let mut server_stream = TcpStream::connect(server_addr).await?;
+    io::copy_bidirectional(&mut client, &mut server_stream).await?;
     Ok(())
 }
