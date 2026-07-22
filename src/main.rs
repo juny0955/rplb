3eca5c632179e880c48762588ebbdf93d16897d4
JunYoung Choi <juny0955@gmail.com>
2026-07-22 13:36:03 +0900

    test: 수동 스모크테스트 진행

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,13 @@ async fn main() -> io::Result<()> {
         SocketAddr::from(([127, 0, 0, 1], 9000)),
         SocketAddr::from(([127, 0, 0, 1], 9001)),
     ];
-    let servers = server_addrs.map(|addr| Server::new(addr, NonZeroU32::MIN));
-    let server_pool = ServerPool::new(Vec::from(servers), LoadBalancingPolicy::RR);
+    let first_server_weight = NonZeroU32::new(3)
+        .ok_or_else(|| io::Error::other("literal server weight should be non-zero"))?;
+    let servers = [
+        Server::new(server_addrs[0], first_server_weight),
+        Server::new(server_addrs[1], NonZeroU32::MIN),
+    ];
+    let server_pool = ServerPool::new(Vec::from(servers), LoadBalancingPolicy::SWRR);
 
     let listener = TcpListener::bind(listen_addr).await?;
 
